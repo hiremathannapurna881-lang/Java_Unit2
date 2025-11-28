0470332790739d85aaf4a4817532381427d5c3ab
Annapurna Sangamesh Hiremath <hiremathannapurna881@gmail.com>
2025-11-28 22:44:41 +0530

    Delete java directory

--- java/ArrayDemo.java
+++
@@ -1,9 +0,0 @@
-public class ArrayDemo {
-    public static void main(String[] args) {
-        int[] scores = {95, 88, 76, 90, 84};
-        System.out.println("Array Elements:");
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println("Index " + i + ": " + scores[i]);
-        }
-    }
-}
